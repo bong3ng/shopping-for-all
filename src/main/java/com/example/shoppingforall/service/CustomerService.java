f9c09a477b41cf211283068c6e1da84c597190bc
AnhVinh <vinh.dang@ntq-solution.com.vn>
2023-10-09 22:17:13 +0700

    Revert "add base shoppingForAll"
    This reverts commit 4c8c3d08

--- src/main/java/com/example/shoppingforall/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoppingforall.service;
-
-import com.example.shoppingforall.domain.Customer;
-
-public interface CustomerService {
-//    Customer getCustomerByid(String id);
-}
